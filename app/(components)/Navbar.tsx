2e784528e1b0f90ea74cc5a633890b25f142e386
Remitito <jrossOT@protonmail.com>
2024-09-02 08:15:27 +0100

    Navbar done

---
+++ app/(components)/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "500",
+});
+
+const Navbar = () => {
+  const LinkStyle = poppins.className;
+
+  return (
+    <nav className="w-screen h-24 flex flex-row justify-between">
+      <Link className="w-1/5 flex items-center h-full" href="/">
+        <img src="/icons/logoEdit.png" alt="Logo" className=" ml-8 h-4/5" />
+      </Link>
+      <div className="w-2/5 h-full flex flex-row justify-evenly items-center">
+        <Link className={LinkStyle} href="login">
+          Login
+        </Link>
+        <Link className={LinkStyle} href="search">
+          Search
+        </Link>
+        <Link className={LinkStyle} href="about">
+          About
+        </Link>
+        <Link className={LinkStyle} href="contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
